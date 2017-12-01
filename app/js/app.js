c3cbf09520f47404895cbdc5a69510cc6ec96e20
evgalp <evgeniyalpayev@gmail.com>
2017-12-01 19:11:45 +0200

    init half of the field from script

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,10 @@
+var appData = (function(){
+  var htmlCode = {
+    cardHtml: '<div class="card"> <div class="flipper"> <div class="flipper__front"> <img src="img/cardbg.jpg" alt="alt"> </div><div class="flipper__back"> <img src="" alt="alt"> </div></div></div>'
+  }
+  return {htmlCode};
+})();
+
 var helpers = (function(){
   var getRandomInt = function (min, max) {
     min = Math.ceil(min);
@@ -5,45 +12,80 @@ var helpers = (function(){
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  return {getRandomInt};
+  var shuffle = function (o) {
+      for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+      return o;
+  };
+
+  var insertMultipleChildren = function(parentId, childrenAmount, childHtml) {
+    var container = document.getElementById(parentId);
+    for ( var counter = 0; counter < childrenAmount; counter++ ) {
+      var div = document.createElement("div");
+      container.appendChild(div);
+      div.outerHTML = childHtml;
+    }
+  }
+
+  return {getRandomInt, shuffle, insertMultipleChildren};
 })();
 
 
 var controller = (function(){
 
-  var cards = document.querySelectorAll(".cardboard .card");
-
-  var flipCard = function() {
-      [].map.call(cards, function(elem) { elem.classList.remove("active") });
-      this.classList.add("card--flipped");
-      var that = this;
-      setTimeout(function(){
-        that.classList.remove("card--flipped")
-      }, 1000)
-  };
-
-  var flipAll = function(){
-    [].map.call(cards, function(elem) {
-        elem.addEventListener("click", flipCard, false);
-    });
-  }
-
-  var initCards = function(){
+  var initCards = function(cardsAmount){
     var cards = document.querySelectorAll('.cardboard .card .flipper .flipper__back img');
 
-    for (var i = 0; i < cards.length; i++) {
-      cards[i].src = `img/img${helpers.getRandomInt(0, 18)}.jpg`
+    var existingImgArr = [];
+
+    for (var i = 0; i < cardsAmount; i++) {
+      existingImgArr.push(i);
     }
+
+    helpers.shuffle(existingImgArr);
+
+    for (var i = 0; i < cards.length; i++) {
+      cards[i].src = `img/img${existingImgArr[i]}.jpg`;
+    }
+
   }
 
-  return {flipAll, initCards}
+  var initField = function (fieldSize) {
+    helpers.insertMultipleChildren('cardboard', fieldSize, appData.htmlCode.cardHtml)
+  }
+
+  var initFlip = function () {
+    var cards = document.querySelectorAll(".cardboard .card");
+
+    var flipCard = function() {
+        [].map.call(cards, function(elem) { elem.classList.remove("active") });
+        this.classList.add("card--flipped");
+        var that = this;
+        setTimeout(function(){
+          that.classList.remove("card--flipped")
+        }, 1000)
+    };
+
+    var flipAll = function(){
+      [].map.call(cards, function(elem) {
+          elem.addEventListener("click", flipCard, false);
+      });
+    }
+
+    flipAll();
+  }
+
+  return {initFlip, initCards, initField}
 })();
 
 function mainLoop(){
-  controller.flipAll();
-  controller.initCards();
-}
+  var cardsToInit = 18;
 
+  controller.initField(cardsToInit);
+  controller.initCards(cardsToInit);
+
+  controller.initFlip();
+
+}
 
 
 window.onload = function() {
