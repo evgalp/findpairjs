9005b66131e5f43eb47241fa7370d7419491fd91
evgalp <evgeniyalpayev@gmail.com>
2017-12-01 15:13:58 +0200

    flip animation

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,39 @@
-function getDom() {
-  var cardboard = document.getElementById('cardboard');
+var model = (function(){
+
+})();
+
+
+var controller = (function(){
+
+  var cards = document.querySelectorAll(".cardboard .card");
+
+  var flipCard = function() {
+      [].map.call(cards, function(elem) { elem.classList.remove("active") });
+      this.classList.add("card--flipped");
+      var that = this;
+      setTimeout(function(){
+        that.classList.remove("card--flipped")
+      }, 1000)
+  };
+
+  var flipAll = function(){
+    [].map.call(cards, function(elem) {
+        elem.addEventListener("click", flipCard, false);
+    });
+  }
+
+  return {flipAll}
+})();
+
+function mainLoop(){
+  controller.flipAll();
 }
 
-function initCardsArray() {
-  var cardsArray = [];
+function initCards(){
+  var cards = document.querySelectorAll('.cardboard .card .flipper .flipper__back img');
+  console.log(cards);
+}
+
+window.onload = function() {
+  mainLoop();
 }
